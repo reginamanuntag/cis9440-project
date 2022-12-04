6b7d65c8ac47b9869188e826c5558b541642a0d0
regina.manuntag@baruchmail.cuny.edu <regina.manuntag@baruchmail.cuny.edu>
2022-12-04 14:42:48 +0000

    location dimension

--- models/marts/core/arrests/fct_arrests.sql
+++ models/marts/core/arrests/fct_arrests.sql
@@ -1,8 +1,11 @@
 select
   Arrest_Key,
   Perpetrator_ID,
-  Offense_ID
+  Offense_ID,
+  Location_ID
 
 from {{ ref('dim_offense_type') }}
 
 left join {{ ref('dim_perpetrator_profile') }} using (Arrest_Key)
+
+left join {{ ref('dim_location') }} using (Arrest_Key)
