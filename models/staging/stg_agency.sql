a69a2f4f070777440b4166b3e52ef8e78cecf69e
Gabriel Green <gabriel.green@baruchmail.cuny.edu>
2022-12-04 17:28:44 +0000

    Added Unique_Key and fixed yml

--- models/staging/stg_agency.sql
+++ models/staging/stg_agency.sql
@@ -1,4 +1,5 @@
 select
     ROW_NUMBER() OVER() AS Agency_ID,
+    Unique_Key as Complaint_ID,
     Agency_Name
-from cis9440-project.311.noise_complaints
+from cis9440-project.311.nyc_noise_complaint
--- models/staging/stg_channel.sql
+++ models/staging/stg_channel.sql
@@ -1,4 +1,5 @@
 select
     ROW_NUMBER() OVER() AS Channel_ID,
+    Unique_Key as Complaint_ID,
     Open_Data_Channel_Type as Channel_Name
-from cis9440-project.311.noise_complaints
+from cis9440-project.311.nyc_noise_complaint
