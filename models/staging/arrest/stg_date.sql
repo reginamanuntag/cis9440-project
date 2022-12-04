2006fa4d9e3bf8ab3c39adccde59dddc3e418ed5
debby.candra@baruchmail.cuny.edu <debby.candra@baruchmail.cuny.edu>
2022-12-04 15:40:17 +0000

    date

--- models/staging/arrest/stg_date.sql
+++ models/staging/arrest/stg_date.sql
@@ -1,46 +1,16 @@
-WITH date_spine AS (
-
-  {{ dbt_utils.date_spine(
-      start_date="to_date('11/01/2009', 'mm/dd/yyyy')",
-      datepart="day",
-      end_date="dateadd(year, 40, current_date)"
-     )
-  }}
-
-), calculated as (
-
-    SELECT
-      date_day,
-      date_day                                                                                AS date_actual,
-
-      DAYNAME(date_day)                                                                       AS day_name,
-
-      DATE_PART('month', date_day)                                                            AS month_actual,
-      DATE_PART('year', date_day)                                                             AS year_actual,
-
-      DATE_PART(dayofweek, date_day) + 1                                                      AS day_of_week,
-
-), current_date_information AS (
-  
-    SELECT 
-      fiscal_year AS current_fiscal_year,
-      first_day_of_fiscal_year AS current_first_day_of_fiscal_year,
-      fiscal_quarter_name_fy AS current_fiscal_quarter_name_fy,
-      first_day_of_month AS current_first_day_of_month
-    FROM calculated
-    WHERE CURRENT_DATE = date_actual
-
-)
-
-SELECT
-  calculated.date_day,
-  calculated.date_actual,
-  calculated.day_name,
-  calculated.month_actual,
-  calculated.year_actual,
-  calculated.day_of_week,
-
-FROM calculated
-CROSS JOIN current_date_information
-
-
+SELECT {{ dbt_utils.surrogate_key(
+     'ARREST_KEY',
+     'ARREST_DATE'
+ ) }}  as Date_ID,
+ 
+ARREST_DATE,
+ 
+Arrest_Key,
+ 
+{{ dbt_date.day_of_month("ARREST_DATE") }} as Date,
+ 
+{{ dbt_date.month_name("ARREST_DATE") }} as Month,
+ 
+cast({{ dbt_date.date_part('year', 'ARREST_DATE') }} as {{ type_int() }}) as Year
+ 
+FROM {{ source('Arrest', 'nyc_arrests') }}
--- models/staging/arrest/stg_location.sql
+++ models/staging/arrest/stg_location.sql
@@ -1,9 +1,13 @@
 select
-    to_hex(md5(cast(coalesce(cast(Zipcode as STRING), '') || '-' || coalesce(cast(ARREST_PRECINCT as STRING), '') as STRING))) as Location_ID,
-    Zipcode,
-    ARREST_BORO,
-    ARREST_PRECINCT,
-    Latitude,
-    Longitude
-
-from cis9440-project.Arrest.nyc_arrests
+   {{ dbt_utils.surrogate_key(
+     'ARREST_KEY',
+     'Zipcode'
+ ) }}  as Location_ID,
+   Arrest_Key,
+   Zipcode,
+   ARREST_BORO,
+   ARREST_PRECINCT,
+   Latitude,
+   Longitude
+ 
+from {{ source('Arrest', 'nyc_arrests') }}
