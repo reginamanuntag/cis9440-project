9ef4cbccf67ee28e8d90b8feeb02b8599a6ea6b1
debby.candra@baruchmail.cuny.edu <debby.candra@baruchmail.cuny.edu>
2022-12-07 18:50:02 +0000

    perp_profile

---
+++ models/staging/arrest/stg_perpetrator_profile.sql
@@ -0,0 +1,11 @@
+select
+    {{ dbt_utils.surrogate_key(
+      'ARREST_KEY',
+      'PERP_SEX'
+  ) }} as Perpetrator_ID,
+    ARREST_KEY as Arrest_Key,
+    PERP_SEX as Sex,
+    PERP_RACE as Race,
+    AGE_GROUP as Age_Group
+
+from {{ source('arrest', 'nyc_arrests') }}
