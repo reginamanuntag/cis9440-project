11774b5ba22efd26fa9d1c50a81d1b004d29c8ff
Gabriel Green <gabriel.green@baruchmail.cuny.edu>
2022-12-07 02:11:12 +0000

    swag

--- models/staging/stg_agency.sql
+++ models/staging/stg_agency.sql
@@ -1,5 +1,5 @@
-select
-    ROW_NUMBER() OVER() AS Agency_ID,
-    Unique_Key as Complaint_ID,
-    Agency_Name
-from cis9440-project.311.nyc_noise_complaint
+select ROW_NUMBER() OVER() AS Agency_ID, *
+from (
+    select distinct Agency_Name
+    from cis9440-project.311.nyc_noise_complaint
+)
--- models/staging/stg_channel.sql
+++ models/staging/stg_channel.sql
@@ -1,5 +1,6 @@
-select
-    ROW_NUMBER() OVER() AS Channel_ID,
-    Unique_Key as Complaint_ID,
-    Open_Data_Channel_Type as Channel_Name
-from cis9440-project.311.nyc_noise_complaint
+select ROW_NUMBER() OVER() AS Channel_ID, *
+from (
+    select distinct
+        Open_Data_Channel_Type as Channel_Name
+    from cis9440-project.311.nyc_noise_complaint
+)
