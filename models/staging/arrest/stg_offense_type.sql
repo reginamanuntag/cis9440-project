c5cee7fc9329a4f378d75147dbf73e7aecb3dd8c
debby.candra@baruchmail.cuny.edu <debby.candra@baruchmail.cuny.edu>
2022-12-07 18:48:32 +0000

    offense type

---
+++ models/staging/arrest/stg_offense_type.sql
@@ -0,0 +1,11 @@
+select
+    {{ dbt_utils.surrogate_key(
+      'ARREST_KEY',
+      'KY_CD'
+  ) }} as Offense_ID,
+    ARREST_KEY as Arrest_Key,
+    OFNS_DESC as Description,
+    KY_CD as Offense_Code,
+    LAW_CAT_CD as Offense_Level 
+
+from {{ source('arrest', 'nyc_arrests') }}
