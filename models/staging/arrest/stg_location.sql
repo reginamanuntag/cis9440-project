7b57b57a8696f4f011b49a2c0b710b2db79917cd
debby.candra@baruchmail.cuny.edu <debby.candra@baruchmail.cuny.edu>
2022-12-07 19:42:19 +0000

    dbt error

--- models/staging/arrest/stg_location.sql
+++ models/staging/arrest/stg_location.sql
@@ -1,28 +1,14 @@
 select
-<<<<<<< HEAD
    {{ dbt_utils.surrogate_key(
      'ARREST_KEY',
      'Zipcode'
  ) }}  as Location_ID,
-   Arrest_Key,
-   Zipcode,
-   ARREST_BORO,
-   ARREST_PRECINCT,
-   Latitude,
-   Longitude
- 
-from {{ source('arrest', 'nyc_arrests') }}
-=======
-    {{ dbt_utils.surrogate_key(
-      'ARREST_KEY',
-      'Zipcode'
-  ) }}  as Location_ID,
     Arrest_Key,
     Zipcode,
     ARREST_BORO,
     ARREST_PRECINCT,
     Latitude,
     Longitude
-
+ 
 from {{ source('arrest', 'nyc_arrests') }}
->>>>>>> d40ac0cb23408d83cee734a4a45abdb862984bea
+
