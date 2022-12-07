84840960937e9b85f42feb76e385021893db98dd
regina.manuntag@baruchmail.cuny.edu <regina.manuntag@baruchmail.cuny.edu>
2022-12-07 17:30:21 +0000

    arrest fact attempt

--- models/marts/core/arrests/fct_arrests.sql
+++ models/marts/core/arrests/fct_arrests.sql
@@ -1,2 +1,26 @@
-select Location_ID
-from {{ ref('dim_location') }}
+with date_dim as (
+    select * from {{ ref('dim_date') }}
+),
+
+location_dim as (
+    select * from {{ ref('dim_location') }}
+),
+
+offense_dim as (
+    select * from {{ ref('dim_offense_type') }}
+),
+
+perp_dim as (
+    select * from {{ ref('dim_perpetrator_profile') }}
+)
+
+select ARREST_KEY,
+Date_ID,
+Location_ID,
+Offense_ID,
+Perpetrator_ID
+from {{ source('arrest', 'nyc_arrests') }}
+left join date_dim on (date_dim.Date_Day=nyc_arrests.Arrest_Date)
+left join location_dim on (location_dim.Latitude=nyc_arrests.Latitude)
+left join offense_dim on (offense_dim.Description=nyc_arrests.OFNS_DESC)
+left join perp_dim on (perp_dim.Race=nyc_arrests.PERP_RACE)
